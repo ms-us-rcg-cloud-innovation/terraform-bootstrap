69865832dea4fcf2c766fa2e165b6d8de42f8f3f
msftph <65667413+msftph@users.noreply.github.com>
2023-01-25 15:01:08 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.26.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+  features {
+
+  }
+}
+
+data "azurerm_client_config" "current" {}
+
+# the resource gropu that contains all triage state
+resource "azurerm_resource_group" "tfstate" {
+  name     = "${var.name}-tfstate"
+  location = var.location
+}
+
+# this random string is used to get a unique storage account name
+# NOTE: terraform state is not saved so this will generate a unique account name each execution
+resource "random_string" "tfstate" {
+  length  = 5
+  special = false
+  upper   = false
+}
+
+resource "azurerm_storage_account" "tfstate" {
+  name                     = "sta${var.name}${lower(random_string.tfstate.result)}"
+  resource_group_name      = azurerm_resource_group.tfstate.name
+  location                 = azurerm_resource_group.tfstate.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.tfstate.name
+  container_access_type = "private"
+}
